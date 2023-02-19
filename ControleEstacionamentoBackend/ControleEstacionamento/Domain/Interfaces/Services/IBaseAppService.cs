b0dbc5c4015b914337ed24d23632aee235918a22
Vinícius <vinigsouza.bnu@gmail.com>
2023-02-19 15:34:27 -0300

    feat: cria serviço base

---
+++ ControleEstacionamentoBackend/ControleEstacionamento/Domain/Interfaces/Services/IBaseAppService.cs
@@ -0,0 +1,17 @@
+﻿using ControleEstacionamento.Domain.Entidades;
+
+namespace ControleEstacionamento.Domain.Interfaces.Services
+{
+    public interface IBaseAppService<TEntity, TDto> where TEntity : EntidadeBase where TDto : class
+    {
+        List<TDto> GetAll();
+
+        TDto GetById(int id);
+
+        TDto Create(TDto dto);
+
+        TDto Update(TDto dto);
+
+        void Delete(int id);
+    }
+}
