92442eaaa222071ac70b32f60a8d1c79397e0b4e
Vinícius <vinigsouza.bnu@gmail.com>
2023-02-19 20:11:32 -0300

    feat: cria serviço de Registro

---
+++ ControleEstacionamentoBackend/ControleEstacionamento/Application/Services/RegistroAppService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using ControleEstacionamento.Application.DTOs;
+using ControleEstacionamento.Domain.Entidades;
+using ControleEstacionamento.Domain.Interfaces.Repositories;
+using ControleEstacionamento.Domain.Interfaces.Services;
+using ControleEstacionamento.Infra.Data;
+
+namespace ControleEstacionamento.Application.Services
+{
+    public class RegistroAppService : BaseAppService<Registro, RegistroDTO>, IBaseAppService<Registro, RegistroDTO>
+    {
+        private readonly ICarroRepository _carroRepository;
+
+        public RegistroAppService(IUnitOfWork unitOfWork, IRepository<Registro> repository, IMapper mapper, ICarroRepository carroRepository) : base(unitOfWork, repository, mapper)
+        {
+            _carroRepository = carroRepository;
+        }
+
+        public override RegistroDTO Create(RegistroDTO dto)
+        {
+            var entidade = _mapper.Map<Registro>(dto);
+            var carro = _carroRepository.GetPorPlaca(dto.PlacaCarro);
+
+            if (carro == null)
+                entidade.Carro = new Carro(dto.PlacaCarro);
+            else
+                entidade.CarroId = carro.Id;
+
+            _repository.Create(entidade);
+            _unitOfWork.Commit();
+
+            return dto;
+        }
+    }
+}
