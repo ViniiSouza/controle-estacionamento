e1bdce9b4548dbe5bc8889f20800881c1bb7b312
Vinícius <vinigsouza.bnu@gmail.com>
2023-02-19 00:16:20 -0300

    feat: cria configuração das entidades

---
+++ ControleEstacionamentoBackend/ControleEstacionamento/Infra.Data/Mappings/CarroMap.cs
@@ -0,0 +1,16 @@
+﻿using ControleEstacionamento.Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ControleEstacionamento.Infra.Data.Mappings
+{
+    public class CarroMap : IEntityTypeConfiguration<Carro>
+    {
+        public void Configure(EntityTypeBuilder<Carro> builder)
+        {
+            builder.HasKey(prop => prop.Id);
+
+            builder.Property(prop => prop.Placa).HasMaxLength(8);
+        }
+    }
+}
