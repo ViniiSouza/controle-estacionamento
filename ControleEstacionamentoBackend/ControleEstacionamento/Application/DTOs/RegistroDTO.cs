2658f7c5beb2383d784e1304c9590019ca2d3b79
Vinícius <vinigsouza.bnu@gmail.com>
2023-02-19 20:05:57 -0300

    feat: adiciona informações à DTO de registro

--- ControleEstacionamentoBackend/ControleEstacionamento/Application/DTOs/RegistroDTO.cs
+++ ControleEstacionamentoBackend/ControleEstacionamento/Application/DTOs/RegistroDTO.cs
@@ -2,5 +2,16 @@
 {
     public class RegistroDTO
     {
+        public int? Id { get; set; }
+
+        public string? PlacaCarro { get; set; }
+
+        public string? HorarioEntrada { get; set; }
+
+        public string? HorarioSaida { get; set; }
+
+        public decimal ValorHora { get; set; }
+
+        public int? CarroId { get; set; }
     }
 }
