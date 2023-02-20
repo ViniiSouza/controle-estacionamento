dd0d3eb25345e2acbd1ee170f806f851e26d7043
Vinícius <vinigsouza.bnu@gmail.com>
2023-02-19 23:00:06 -0300

    feat: configura injeções de dependência

--- ControleEstacionamentoBackend/ControleEstacionamentoAPI/Program.cs
+++ ControleEstacionamentoBackend/ControleEstacionamentoAPI/Program.cs
@@ -1,5 +1,10 @@
 using ControleEstacionamento.Application.Mappers.Profiles;
+using ControleEstacionamento.Application.Services;
+using ControleEstacionamento.Domain.Interfaces.Repositories;
+using ControleEstacionamento.Domain.Interfaces.Services;
+using ControleEstacionamento.Infra.Data;
 using ControleEstacionamento.Infra.Data.Context;
+using ControleEstacionamento.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +16,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(RegistroProfile));
+
+// Dependency Injection
+
+builder.Services.AddTransient(typeof(IUnitOfWork), typeof(UnitOfWork));
+builder.Services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddTransient(typeof(ICarroRepository), typeof(CarroRepository));
+builder.Services.AddTransient(typeof(IBaseAppService<,>), typeof(BaseAppService<,>));
+builder.Services.AddTransient(typeof(IRegistroAppService), typeof(RegistroAppService));
 
 builder.Services.AddDbContext<ControleEstacionamentoDbContext>(options =>
 {
