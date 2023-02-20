5300b804c9c638f7f8100aa0f9baf68dabffc12d
Vinícius <vinigsouza.bnu@gmail.com>
2023-02-20 13:59:46 -0300

    feat: inclui migrations

---
+++ ControleEstacionamentoBackend/ControleEstacionamento/Migrations/20230220164334_TabelaEntidades.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControleEstacionamento.Migrations
+{
+    /// <inheritdoc />
+    public partial class TabelaEntidades : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Placa = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carro", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VigenciaPreco",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InicioVigencia = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FimVigencia = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ValorHora = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VigenciaPreco", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registro",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HorarioEntrada = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HorarioSaida = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ValorHora = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    CarroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registro", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Registro_Carro_CarroId",
+                        column: x => x.CarroId,
+                        principalTable: "Carro",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registro_CarroId",
+                table: "Registro",
+                column: "CarroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registro");
+
+            migrationBuilder.DropTable(
+                name: "VigenciaPreco");
+
+            migrationBuilder.DropTable(
+                name: "Carro");
+        }
+    }
+}
