9ddbfd6bbcb4cdc135ded8fd8558e454e3d32c22
Vinícius <vinigsouza.bnu@gmail.com>
2023-02-19 23:46:41 -0300

    feat: cria base do projeto frontend

---
+++ controle-estacionamento-frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Header from "./components/header/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+}
+</script>
+
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ controle-estacionamento-frontend/src/components/header/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+            <h2 class="header-title">Estacionamento Carro Bem Guardado</h2>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.header {
+    width: 100%;
+    background-color: rgb(11, 2, 94);
+    height: 10vh;
+}
+.header-image{
+    width: 97px;
+    margin-left: 1vw;
+    display: inline;
+}
+.header-title {
+    color: white;
+    text-align: center;
+    font-size: 2rem;
+    line-height: 10vh;
+}
+</style>
